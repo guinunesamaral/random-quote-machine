ea5b707348ee9fde3dfe78c4d2b1adddef7f01d5
guilherme nunes <guinucode@protonmail.com>
2022-03-06 12:00:37 -0300

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { CSSTransition } from "react-transition-group";
+import "./App.css";
 
 function App() {
+  const [quote, setQuote] = useState("");
+  const [author, setAuthor] = useState("");
+  const [showContent, setShowContent] = useState(true);
+  const [isFetching, setIsFetching] = useState(false);
+
+  useEffect(() => {
+    fetchQuote();
+  }, []);
+
+  const fetchQuote = () => {
+    fetch("https://quotes15.p.rapidapi.com/quotes/random/", {
+      headers: {
+        "x-rapidapi-host": "quotes15.p.rapidapi.com",
+        "x-rapidapi-key": "2a0f2122ccmsh4c64d2cef70c8c6p1f96acjsnbd8e67cb13d7",
+      },
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        if (res && res.originator) {
+          setQuote(res.content);
+          setAuthor(res.originator.name);
+        }
+      });
+  };
+
+  const handleClick = () => {
+    if (!isFetching) {
+      fetchQuote();
+      setIsFetching(true);
+      setShowContent(false);
+      setTimeout(() => {
+        setIsFetching(false);
+        setShowContent(true);
+      }, 1000);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={handleClick}>New quote</button>
+      <CSSTransition
+        in={showContent}
+        timeout={1000}
+        classNames="content"
+        unmountOnExit
+      >
+        <div>
+          <q>{quote}</q>
+          <p>{author}</p>
+        </div>
+      </CSSTransition>
     </div>
   );
 }
